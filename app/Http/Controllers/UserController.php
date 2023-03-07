88adf336cd73f13f344a13227115e7af9e2809ad
kishor shrestha <kisshor916@gmail.com>
2023-03-07 17:47:04 +1100

    search almost complet

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,6 +75,57 @@ class UserController extends Controller
     
         return 'User has been registered';
     }
+
+    public function search(Request $request)
+{
+    $incomingFields = $request->validate([
+        'gender' => 'required',
+        'min_age' => 'nullable|integer',
+        'max_age' => 'nullable|integer',
+        'name' => 'nullable',
+        'distance' => 'nullable|integer',
+    ]);
+
+    $query = User::query();
+
+    // Filter by gender
+    $query->where('gender', $incomingFields['gender']);
+
+    // Filter by age range
+    if (isset($incomingFields['min_age'])) {
+        $query->whereRaw('YEAR(CURRENT_DATE) - YEAR(date_of_birth) >= ?', [$incomingFields['min_age']]);
+    }
+    if (isset($incomingFields['max_age'])) {
+        $query->whereRaw('YEAR(CURRENT_DATE) - YEAR(date_of_birth) <= ?', [$incomingFields['max_age']]);
+    }
+
+    // Filter by name
+    if (isset($incomingFields['name'])) {
+        $query->where(function($q) use ($incomingFields) {
+            $q->where('first_name', 'LIKE', '%'.$incomingFields['name'].'%')
+              ->orWhere('last_name', 'LIKE', '%'.$incomingFields['name'].'%');
+        });
+    }
+
+    // Filter by distance
+    if (isset($incomingFields['distance'])) {
+        $origin = auth()->user()->latitude.',' .auth()->user()->longitude;
+        $haversine = '( 6371 * acos( cos( radians('.$origin.') ) *
+                cos( radians( latitude ) )
+                * cos( radians( longitude ) - radians('.$origin.') )
+                + sin( radians('.$origin.') ) *
+                sin( radians( latitude ) ) ) )';
+        $query->selectRaw("*, {$haversine} AS distance")
+              ->having('distance', '<=', $incomingFields['distance'])
+              ->orderBy('distance', 'asc');
+    }
+
+    $results = $query->get();
+
+    return view('search-results', compact('results'));
+}
+
+
     
 
     public function profile()
